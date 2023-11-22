c31ba33bea2773f4019972e76f743f53b67ad540
Lalo <Gonzalo.alvalon@gmail.com>
2023-11-22 13:22:29 +0100

    Change color

--- src/components/PlaylistCard.tsx
+++ src/components/PlaylistCard.tsx
@@ -1,22 +1,28 @@
 // En un nuevo archivo llamado Playlist.tsx
-import React from 'react';
-import Image from 'next/image';
+import React from 'react'
+import Image from 'next/image'
 import type { Playlist } from '@/lib/spotify/types'
 
 interface PlaylistProps {
-    playlist: Playlist; 
-    onPlaylistClick: (playlist: any) => void; 
-    selectedPlaylists: string[];
+    playlist: Playlist
+    onPlaylistClick: (playlist: any) => void
+    selectedPlaylists: string[]
 }
 
-const PlaylistCard: React.FC<PlaylistProps> = ({ playlist, onPlaylistClick, selectedPlaylists }) => {
+
+
+const PlaylistCard: React.FC<PlaylistProps> = ({
+    playlist,
+    onPlaylistClick,
+    selectedPlaylists,
+}) => {
     return (
         <div
             key={playlist.id}
-            className={`flex gap-4 cursor-pointer bg-[#242424] rounded-md p-3 border-2 ${
+            className={`playList flex gap-4 cursor-pointer bg-[#242424] rounded-md p-3 border-2 ${
                 selectedPlaylists.includes(playlist.id)
                     ? 'border-white'
-                    : 'border-[#0c0c0c] hover:border-[#1a1a1a]'
+                    : 'border-[#0c0c0c] hover:bg-[#383838]'
             } transition ${
                 parseInt(playlist.tracks.total) === 0
                     ? 'opacity-50 pointer-events-none'
@@ -36,15 +42,13 @@ const PlaylistCard: React.FC<PlaylistProps> = ({ playlist, onPlaylistClick, sele
                 className="rounded-md"
             />
             <div className="grow">
-                <p className="font-semibold text-white">
-                    {playlist.name}
-                </p>
+                <p className="font-semibold text-white">{playlist.name}</p>
                 <p className="text-xs text-gray-400 font-semibold">
                     {playlist.tracks.total} songs
                 </p>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default PlaylistCard;
